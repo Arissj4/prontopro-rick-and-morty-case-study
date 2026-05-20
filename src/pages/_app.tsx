618a79838bd6dec79b5229e3baf9e6e297f3677b
Soheil Jamshidi <eris1376@gmail.com>
2026-05-21 01:05:12 +0200

    R&M [ADD] Modified app page architecture

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import type { AppProps } from 'next/app'
-import '~/styles/globals.css'
+import '@/styles/globals.css'
+import Navbar from '../components/navbar'
 
-const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps) => {
+  return(
+    <>
+      <div className='w-md m-auto h-screen'>
+        <Navbar />
+
+        <main className="pt-17.5">
+          <Component {...pageProps}/>
+        </main>
+      </div>
+    </>
+  )
+}
 
 export default App
