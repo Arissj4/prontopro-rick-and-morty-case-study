aac12854b2f84d6a809e05f11568cde19f88300b
Soheil Jamshidi <eris1376@gmail.com>
2026-05-21 09:10:51 +0200

    R&M [Refactor] Refactored Navbar component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import '@/styles/globals.css'
-import Navbar from '../components/navbar'
+import '@/styles/globals.scss'
+import Navbar from '../components/NavbarComponent'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return(
