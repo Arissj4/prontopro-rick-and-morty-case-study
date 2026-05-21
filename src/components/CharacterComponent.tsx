8147d8ceb75454a1246587b3a0c304d24fb5370c
Soheil Jamshidi <eris1376@gmail.com>
2026-05-21 09:15:57 +0200

    R&M [FEAT] Implemented Character component, implemented filter and character components inside the home page

---
+++ src/components/CharacterComponent.tsx
@@ -0,0 +1,16 @@
+import type { Character } from "@/lib/costumeTypes";
+
+
+export default function CharacterComponent({character}: {character: Character}) {
+  return(
+    <>
+      <div className="character-card">
+        <img className="character-card__image" src={character.image} alt={character.name}/>
+        <div className="character-card__info">
+          <h2>{character.name}</h2>
+          <h4>{character.species}</h4>
+        </div>
+      </div>
+    </>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,43 @@
 import RandMPicture from "@/public/rick-and-morty.svg"
 import Image from "next/image"
+import Filter from "@/components/FilterComponent"
+import type { Character } from "@/lib/costumeTypes"
+import CharacterComponent from "@/components/CharacterComponent"
 
+type HomeProps = {
+	characters: Character[];
+}
 
+const Home = ({ characters }: HomeProps) => {
+	return (
+		<>
+			<div className='page flex flex-col items-center'>
+				<Image src={RandMPicture} alt="Rick and Morty Logo" />
 
-const Home = () => (
-	<>
-		<div className='page flex flex-col items-center'>
-			<Image src={RandMPicture} alt="Rick and Morty Logo" />
+				<Filter advancedButton={true}/>
 
-			<div id="filters-container" className="">
-				<input type="text" placeholder="Filter by name..."/>
+				<section className="w-full flex flex-col items-center">
+					{characters.map(character => (
+						<CharacterComponent character={character} key={character.id} />
+					))}
+				</section>
+
 			</div>
-		</div>
-	</>
-)
+		</>
+	)
+}
 
-export default Home
+export async function getServerSideProps () {
+
+	const res = await fetch("https://rickandmortyapi.com/api/character");
+
+	const data = await res.json();
+
+	return {
+		props: {
+			characters: data.results,
+		}
+	}
+}
+
+export default Home;
