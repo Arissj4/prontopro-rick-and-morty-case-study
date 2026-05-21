de80820189e289db3c2d552a2b884e5b56937d28
Soheil Jamshidi <eris1376@gmail.com>
2026-05-21 11:02:18 +0200

    R&M [FIX] Fixed loading pictures

--- src/components/CharacterComponent.tsx
+++ src/components/CharacterComponent.tsx
@@ -5,7 +5,7 @@ export default function CharacterComponent({character}: {character: Character}) 
   return(
     <>
       <div className="character-card">
-        <img className="character-card__image" src={character.image} alt={character.name}/>
+        <img className="character-card__image" src={character.image} alt={character.name} loading="lazy"/>
         <div className="character-card__info">
           <h2>{character.name}</h2>
           <h4>{character.species}</h4>
