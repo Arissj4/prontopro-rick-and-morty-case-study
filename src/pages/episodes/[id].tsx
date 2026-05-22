59a0d84428872bac85bc4c6df4773dc4e225a84e
Soheil Jamshidi <eris1376@gmail.com>
2026-05-22 15:17:44 +0200

    R&M [FEAT] Implemented Episodes page, Implemented episode details page

---
+++ src/pages/episodes/[id].tsx
@@ -0,0 +1,86 @@
+import { RMCharacter, RMEpisode } from "@/lib/costumeTypes";
+import { useState } from "react";
+import { GetServerSideProps } from "next";
+import Image from "next/image"
+import Link from "next/link";
+import Back from "@/public/back.svg"
+import CharacterComponent from "@/components/CharacterComponent";
+
+type ProfileProps = {
+  data: RMEpisode,
+  charactersData: RMCharacter[]
+}
+
+export default function Profile({ data, charactersData }: ProfileProps){
+  const [episode, setLocation] = useState<RMEpisode>(data)
+  const [characters, setResidents] = useState<RMCharacter[]>(charactersData);
+
+
+  return(
+    <>
+      <div className="p-4">
+        <div>
+          <Link
+            className="flex hover:cursor-pointer"
+            href={"/episodes/"}
+          >
+            <Image src={Back} alt="Go back" width={24} height={24}/>
+            <span className="ml-2">Go Back</span>
+          </Link>
+        </div>
+
+        <section className="locationInfo-container">
+          <div className="locationInfo-container__title">
+            <h1 className="text-center text-[32px] font-normal mt-4">{episode.name}</h1>
+          </div>
+
+          <div className="locationInfo-container__type">
+            <span>
+              Episode
+            </span>
+            <span>
+              Date
+            </span>
+          </div>
+
+          <div className="locationInfo-container__dimension">
+            <span>
+              {episode.episode}
+            </span>
+            <span>
+              {episode.air_date}
+            </span>
+          </div>
+        </section>
+
+        <section className="residents-container">
+          <h2 className="residents-container__title">
+            Cast
+          </h2>
+
+          {characters.map(character => (
+            <CharacterComponent character={character} key={character.id} />
+          ))}
+        </section>
+      </div>
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+
+  const res = await fetch(`https://rickandmortyapi.com/api/episode/${context.query.id}`)
+  const data = await res.json()
+
+
+  const charactersQueryIds = data.characters.map((episode: string) => episode.split('/').pop());
+  const query: string = charactersQueryIds.join();
+  const charactersRes = await fetch(`https://rickandmortyapi.com/api/character/${query}`)
+  const charactersData = await charactersRes.json()
+  return {
+    props: {
+      data,
+      charactersData
+    }
+  }
+}
---
+++ src/pages/episodes/index.tsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import Image from "next/image";
+import RM from "@/public/R&M.svg"
+import type { RMEpisode } from "@/lib/costumeTypes";
+import { GetServerSideProps } from "next"
+import Filter from "@/components/FilterComponent";
+import Pagination from "@/components/PaginationComponents";
+import SimpleCardComponent from "@/components/SimpleCardComponent";
+
+
+type EpisodesProps = {
+	initCharacters: RMEpisode[],
+	initNextPage: string,
+}
+
+export default function Episodes({ initCharacters, initNextPage }: EpisodesProps) {
+
+  const [episodes, setLocations] = useState<RMEpisode[]>(initCharacters);
+	const [nextPage, setNextPage] = useState<string | null>(initNextPage);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  return(
+    <>
+      {isLoading && (
+				<div className="fixed z-60 w-md flex h-full justify-center items-center bg-white opacity-60 text-black text-[24px] ">
+					Loading...
+				</div>
+			)}
+
+			<div className={`page flex flex-col items-center`}>
+				<Image src={RM} alt="Rick and Morty Logo" />
+
+				<Filter
+					searchPlaceholder="Name or episode (ex.S01E01)..."
+					initURL={'https://rickandmortyapi.com/api/episode'}
+					nextPage={nextPage}
+					setData={setLocations}
+					setNextPage={setNextPage}
+					setIsLoading={setIsLoading}
+					advancedButton={false}
+				/>
+
+				<section className="w-full flex flex-col items-center">
+					{episodes.length > 0 ?
+						(episodes?.map(episode => (
+							<SimpleCardComponent type={"episode"} data={episode} key={episode.id} />
+						)))
+					:
+						<div className="text-[18px] font-medium">
+							No Locations found.
+						</div>
+					}
+				</section>
+
+				{episodes.length > 0 && nextPage && (
+					<Pagination<RMEpisode> nextPage={nextPage} setData={setLocations} setNextPage={setNextPage} setIsLoading={setIsLoading}/>
+				)}
+			</div>
+    </>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps<EpisodesProps> = async () => {
+
+  const res = await fetch(`https://rickandmortyapi.com/api/episode`);
+
+  const data = await res.json();
+
+  return {
+    props: {
+      initCharacters: data.results,
+      initNextPage: data.info.next,
+    }
+  }
+}
