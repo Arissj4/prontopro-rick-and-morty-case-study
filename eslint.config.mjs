5874f725b6a3e8359dfaf24bae16ed7525a1c734
Soheil Jamshidi <eris1376@gmail.com>
2026-05-22 17:26:17 +0200

    R&M [CHORE] configure ESLint

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from "eslint/config";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+
+export default defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  {
+    ignores: [
+      ".next/**",
+      "out/**",
+      "build/**",
+      "next-env.d.ts",
+      "node_modules/**",
+    ],
+  },
+]);
