36fa72154c3aac8821a7c28341a9e62c5262c534
Soheil Jamshidi <eris1376@gmail.com>
2026-05-23 16:49:44 +0200

    R&M [FIX] Changed div to link

--- src/components/SimpleCardComponent.tsx
+++ src/components/SimpleCardComponent.tsx
@@ -1,5 +1,5 @@
 import { RMEpisode, RMLocation } from "@/lib/costumeTypes";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 type SimpleCardProps =
 	| {
@@ -12,41 +12,30 @@ type SimpleCardProps =
 	  };
 
 export default function SimpleCardComponent({ type, data }: SimpleCardProps) {
-	const router = useRouter();
 	return (
 		<>
 			{type === "location" ? (
-				<div
+				<Link
 					className="simple-card-card"
-					onClick={() => router.push(`/locations/${data.id}`)}
+					href={`/locations/${data.id}`}
 				>
 					<div className="simple-card-card__info">
 						<h2>{data.name}</h2>
 						<h4>{data.type}</h4>
 					</div>
-				</div>
+				</Link>
 			) : type === "episode" ? (
-				<div
+				<Link
 					className="simple-card-card"
-					onClick={() => router.push(`/episodes/${data.id}`)}
+					href={`/episodes/${data.id}`}
 				>
 					<div className="simple-card-card__info">
 						<h2>{data.name}</h2>
 						<h4>{data.air_date}</h4>
 						<h3>{data.episode}</h3>
 					</div>
-				</div>
+				</Link>
 			) : null}
-			{/* <div
-        className="simple-card-card"
-        onClick={() => router.push(`/profile/${character.id}`)}
-      >
-        <img className="character-card__image" src={character.image} alt={character.name} loading="lazy"/>
-        <div className="character-card__info">
-          <h2>{character.name}</h2>
-          <h4>{character.species}</h4>
-        </div>
-      </div> */}
 		</>
 	);
 }
