d44ecaa50b63faa5eec78201dd5919809baacfe3
Soheil Jamshidi <eris1376@gmail.com>
2026-05-24 09:20:56 +0200

    R&M [TEST] Implemented new tests for NavbarComponent

--- src/components/NavbarComponent.test.tsx
+++ src/components/NavbarComponent.test.tsx
@@ -1,5 +1,5 @@
 import Navbar from "./NavbarComponent";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 const mockPush = jest.fn();
 
@@ -20,5 +20,24 @@ describe("Check Navbar rendering and routing", () => {
 		const toggleButton = screen.getByRole("button", { name: "Toggle menu" });
 
 		expect(toggleButton).toBeInTheDocument();
+	});
+
+	it("Opens the menu when toggle button is clicked", () => {
+		render(<Navbar />);
+
+		fireEvent.click(screen.getByRole("button", { name: "Toggle menu" }));
+
+		expect(screen.getByText("Characters")).toBeInTheDocument();
+		expect(screen.getByText("Locations")).toBeInTheDocument();
+		expect(screen.getByText("Episodes")).toBeInTheDocument();
+	});
+
+	it("Goes to Locations page, then Locations is clicked", () => {
+		render(<Navbar />);
+
+		fireEvent.click(screen.getByRole("button", { name: "Toggle menu" }));
+		fireEvent.click(screen.getByText("Locations"));
+
+		expect(mockPush).toHaveBeenCalledWith("/locations");
 	});
 });
